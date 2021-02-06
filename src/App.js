719be1cfb3bfd82dea51b669b622f6362f15c77f
Frenzy265 <klaus_franziska@gmx.de>
2021-02-06 19:27:54 +0100

    🔥 Remove starter code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import GlobalStyle from "./globalsstyle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <GlobalStyle />
+      <header>
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
+    </>
   );
 }
 
