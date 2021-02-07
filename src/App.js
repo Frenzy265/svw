95c919698392de825e0228a3e7a125aae6575529
Frenzy265 <klaus_franziska@gmx.de>
2021-02-07 16:52:55 +0100

    💄 Create page intro and add to app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import logo from "./logo.svg";
 import GlobalStyle from "./globalsstyle";
+import { Intro } from "./pages/Intro";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <header>
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Intro />
     </>
   );
 }
---
+++ src/pages/Intro.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import { SongList } from "../components/Songlist";
+import { Menue } from "../components/Menue";
+
+export const Intro = () => {
+  return (
+    <>
+      <Header>
+        <h1>weserstadion</h1>
+        <h2>für dein zuhause</h2>
+      </Header>
+      <SongList />
+      <Menue />
+    </>
+  );
+};
