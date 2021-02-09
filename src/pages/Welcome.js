5a2818b9e7a8999d64b888aa4f47b7a27b5a8349
Frenzy265 <klaus_franziska@gmx.de>
2021-02-09 16:57:43 +0100

    ✨ Add page welcome

---
+++ src/pages/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Textbox } from "../components/Textbox";
+
+export const Welcome = () => {
+  return (
+    <>
+      <Textbox
+        title={"Moin!"}
+        subtitle={"Willkommen im weserstadion für dein zu Hause."}
+      />
+    </>
+  );
+};
