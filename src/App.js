d11650c8f6827e0859ef01e9fb4468c3c8df061b
Frenzy265 <klaus_franziska@gmx.de>
2021-02-19 12:16:56 +0100

    ✨ Add gif page at app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import GlobalStyle from "./globalsstyle";
+import { Giphy } from "./pages/Giphy";
 import { Music } from "./pages/Music";
 import { Welcome } from "./pages/Welcome";
 
@@ -16,6 +17,9 @@ function App() {
           </Route>
           <Route path="/music">
             <Music />
+          </Route>
+          <Route path="/giphy">
+            <Giphy />
           </Route>
         </Switch>
       </Router>
