cd93009eb56cb49377808e61d837197a6fbd6d7d
Frenzy265 <klaus_franziska@gmx.de>
2021-02-19 12:16:34 +0100

    ✨ create gif page

---
+++ src/pages/Giphy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import { GifList } from "../components/List";
+import { Menue } from "../components/Menue";
+
+export const Giphy = () => {
+  const name = localStorage.getItem("Name");
+  return (
+    <>
+      <Header>
+        <div>
+          <h1>emotionen</h1>
+          <h2>für {name}´s zuhause.</h2>
+        </div>
+      </Header>
+      <GifList />
+      <Menue />
+    </>
+  );
+};
