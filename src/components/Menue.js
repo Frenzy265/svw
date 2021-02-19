f62b284b93ade00bc37ac96d49f2b597f45ee714
Frenzy265 <klaus_franziska@gmx.de>
2021-02-19 12:45:51 +0100

    ⚡️ Add link to menue

--- src/components/Menue.js
+++ src/components/Menue.js
@@ -1,17 +1,28 @@
 import styled from "styled-components/macro";
 import Raute from "../assets/raute-transparent.gif";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const Image = styled.img`
   position: fixed;
   bottom: 15px;
   right: 15px;
   height: 130px;
+  width: 100px;
+  background: none;
+  border: none;
 `;
 
-export const Menue = () => {
+export const Menue = ({ path }) => {
   return (
     <>
-      <Image src={Raute} alt="Werder Raute" />
+      <Link to={path}>
+        <Image src={Raute} alt="Werder Raute" />
+      </Link>
     </>
   );
+};
+
+Menue.propTypes = {
+  path: PropTypes.string.isRequired,
 };
