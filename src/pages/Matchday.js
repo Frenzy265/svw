60c8bd2844c4d9c159c8dcd06b465fad276442d4
Frenzy265 <klaus_franziska@gmx.de>
2021-03-19 20:17:13 +0100

    ⚡️ Update data

--- src/pages/Matchday.js
+++ src/pages/Matchday.js
@@ -3,12 +3,14 @@ import { useQuery } from "react-query";
 import Header from "../components/Header";
 import { Menue } from "../components/Menue";
 import Schedule from "../components/Schedule/index";
+import { MatchesContainer } from "../components/Schedule/scheduleElements";
 import { getMatchday } from "../data/matchday";
 
 export const Matchday = () => {
   const name = localStorage.getItem("Name");
-
   const { data: matches, status } = useQuery("matches", getMatchday);
+
+  // console.log(matches[7]);
 
   return (
     <>
@@ -18,19 +20,23 @@ export const Matchday = () => {
         <>
           <Header>
             <div>
-              <h1>{matches[1]?.Group.GroupOrderID}. Spieltach!</h1>
+              <h1>{matches[0]?.Group.GroupOrderID}. Spieltach!</h1>
               <h2>für {name}´s zuhause.</h2>
             </div>
           </Header>
-          {matches?.map((match, index) => (
-            <Schedule
-              key={index}
-              team1={match?.Team1.TeamName}
-              logo1={match?.Team1.TeamIconUrl}
-              team2={match?.Team2.TeamName}
-              logo2={match?.Team2.TeamIconUrl}
-            />
-          ))}
+          <MatchesContainer>
+            {matches?.map((match, index) => (
+              <Schedule
+                key={index}
+                team1={match.Team1.ShortName}
+                logo1={match.Team1.TeamIconUrl}
+                team2={match.Team2.ShortName}
+                logo2={match.Team2.TeamIconUrl}
+                resultTeam1={match.MatchResults[0]?.PointsTeam1}
+                resultTeam2={match.MatchResults[0]?.PointsTeam2}
+              />
+            ))}
+          </MatchesContainer>
           <Menue path="/giphy" />
         </>
       )}
