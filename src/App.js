9bdce2da26c06de30d2d9bbf23c2c587167e1b00
Frenzy265 <klaus_franziska@gmx.de>
2021-03-21 13:37:44 +0100

    ✨ Add redirection if name is already given

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import GlobalStyle from "./globalsstyle";
 import { Giphy } from "./pages/Giphy";
@@ -10,6 +15,7 @@ import NextMatch from "./pages/NextMatch";
 
 function App() {
   const queryClient = new QueryClient();
+  const name = localStorage.getItem("Name");
 
   return (
     <>
@@ -18,7 +24,7 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/">
-              <Welcome />
+              {name ? <Redirect to="/music" /> : <Welcome />}
             </Route>
             <Route path="/music">
               <Music />
